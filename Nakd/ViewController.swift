62fb5d04407e446bc6d5dfca60ee34fe43271875
Ali Fayed <ali.fayed@fixedmea.com>
2021-11-01 14:10:55 +0200

    Initial Commit

---
+++ Nakd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nakd
+//
+//  Created by Ali Fayed on 01/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
