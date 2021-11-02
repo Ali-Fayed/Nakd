1876d1a860f7883eedf0f1f34b8fb48dd21c7e49
Ali Fayed <ali.fayed@fixedmea.com>
2021-11-02 03:43:14 +0200

    home view with layout

--- Nakd/Modules/Home Module/Models/Constants.swift
+++ Nakd/Modules/Home Module/Models/Constants.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 struct Links {
-    static let videoLink = "https://r2---sn-4g5ednss.googlevideo.com/videoplayback?expire=1635799246&ei=bvx_Ya_hBcaQsfIPsOCRgA0&ip=23.224.174.34&id=o-ACHzP8UUwt7sdXfABOPNLRtRFdFU4Q2b3yUU_vGlTfPx&itag=18&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ns=DjNw8cxGdq2mVe7V_De6ZGUG&gir=yes&clen=35952557&ratebypass=yes&dur=1514.173&lmt=1611057100091020&fexp=24001373,24007246&c=WEB&txp=5311224&n=WigcM1s2T_myig&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIhANfsHWSPmafcJuFBuIQvaYkJl0yA25b8rYOJq9I9gTg3AiBPQaH6ke5iixFQ10YZNsUYwT36kOm-KnoVOIgos2-wIg%3D%3D&redirect_counter=1&cm2rm=sn-a5mky7l&req_id=6a78cdffd51aa3ee&cms_redirect=yes&mh=as&mip=197.61.54.31&mm=34&mn=sn-4g5ednss&ms=ltu&mt=1635778357&mv=m&mvi=2&pl=22&lsparams=mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIgH-E0zuZj4kZrrXWAp2m383bLNeZiAKQCLtpbunPzIAUCIQDsclEluZK67DqT7AqLx87kdMYoS3FPo6qYHFtlkJL4uQ%3D%3D"
+    static let videoLink = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
 }
--- Nakd/Modules/Home Module/ViewController/HomeViewController.swift
+++ Nakd/Modules/Home Module/ViewController/HomeViewController.swift
@@ -11,19 +11,27 @@ import AVFoundation
 
 class HomeViewController: UIViewController, AVPlayerViewControllerDelegate {
     //MARK: - Props
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var playVideoButton: UIButton!
+    var imgArr = [  UIImage(named:"101"),
+                    UIImage(named:"102") ,
+                    UIImage(named:"101") ,
+                    UIImage(named:"102") ,
+    ]
     lazy var viewModel = HomeViewModel()
     var playerController = AVPlayerViewController()
+    var timer = Timer()
+    var counter = 0
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         UIconfig ()
         tableViewConfig ()
+        autoScrollcollectionConfig ()
     }
     //MARK: - UI methods
     func UIconfig () {
-        playVideoButton.layer.cornerRadius = 10
         title = "Home".localized()
     }
     func tableViewConfig () {
@@ -32,15 +40,29 @@ class HomeViewController: UIViewController, AVPlayerViewControllerDelegate {
         tableView.registerCellNib(cellClass: CategoriesTableViewCollectionCell.self)
         tableView.registerCellNib(cellClass: OthersTableViewCollectionCell.self)
     }
-    @IBAction func playVideoButtonPressed(_ sender: UIButton) {
-        guard let url = URL(string: Links.videoLink) else {return}
-        let player = AVPlayer(url: url)
-        playerController = AVPlayerViewController()
-        playerController.player = player
-        playerController.allowsPictureInPicturePlayback = true
-        playerController.delegate = self
-        playerController.player?.play()
-        self.present(playerController, animated: true, completion: nil)
+    //MARK: - AutoScrollConfig
+    func autoScrollcollectionConfig () {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        pageControl.numberOfPages = imgArr.count
+        pageControl.currentPage = 0
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
+        }
+    }
+    @objc func changeImage() {
+        if counter < imgArr.count {
+            let index = IndexPath.init(item: counter, section: 0)
+            self.collectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+            pageControl.currentPage = counter
+            counter += 1
+        } else {
+            counter = 0
+            let index = IndexPath.init(item: counter, section: 0)
+            self.collectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: false)
+            pageControl.currentPage = counter
+            counter = 1
+        }
     }
 }
 //MARK: - TableView
@@ -51,15 +73,22 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        let myLabel = UILabel()
+        myLabel.frame = CGRect(x: 65, y: 0, width: 320, height: 20)
+        myLabel.font = UIFont.myBoldSystemFont(ofSize: 20)
         switch section {
         case 0:
-            return "Categories".localized()
+            myLabel.text = "Categories".localized()
         case 1:
-            return "Others".localized()
+            myLabel.text = "Others".localized()
         default:
-            return ""
+            myLabel.text = ""
         }
+        let headerView = UIView()
+        headerView.addSubview(myLabel)
+        return headerView
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.section {
@@ -81,10 +110,54 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
         return IndexPath()
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120
+        return 180
     }
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         return UIView()
     }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 30
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
     
+}
+//MARK: - CollectionView
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgArr.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AutoScrollCell", for: indexPath)
+        if let vc = cell.viewWithTag(111) as? UIImageView {
+            vc.image = imgArr[indexPath.row]
+        }
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+            guard let url = URL(string: Links.videoLink) else {return}
+            let player = AVPlayer(url: url)
+            playerController = AVPlayerViewController()
+            playerController.player = player
+            playerController.allowsPictureInPicturePlayback = true
+            playerController.delegate = self
+            playerController.player?.play()
+            self.present(playerController, animated: true, completion: nil)
+    }
+}
+//MARK: - CollectionView Layout
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = self.collectionView.frame.size
+        return CGSize(width: size.width, height: size.height)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
 }
