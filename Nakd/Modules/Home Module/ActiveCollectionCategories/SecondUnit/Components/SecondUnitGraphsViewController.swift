6951b247c7b96a95eda0a9dcd88434ae1799c238
Ali Fayed <ali.fayed@fixedmea.com>
2021-11-03 16:13:58 +0200

    add graphs vis

--- Nakd/Modules/Home Module/ActiveCollectionCategories/SecondUnit/Components/SecondUnitGraphsViewController.swift
+++ Nakd/Modules/Home Module/ActiveCollectionCategories/SecondUnit/Components/SecondUnitGraphsViewController.swift
@@ -5,25 +5,34 @@
 //  Created by Ali Fayed on 02/11/2021.
 //
 
+import Charts
 import UIKit
-
-class SecondUnitGraphsViewController: UIViewController {
-
-    override func viewDidLoad() {
+class SecondUnitGraphsViewController: UIViewController, ChartViewDelegate {
+    var pieChart = PieChartView()
+    var entries = [ChartDataEntry]()
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
         title = "EducationGraphs".localized()
-        // Do any additional setup after loading the view.
+        pieChart.delegate = self
+        chartsData ()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        pieChart.frame = CGRect(x: 0, y: 0,
+                                width: self.view.frame.size.width,
+                                height: self.view.frame.size.width)
+        pieChart.center = view.center
+        view.addSubview(pieChart)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func chartsData () {
+        for x in 0..<10 {
+            entries.append (ChartDataEntry(x: Double(x), y: Double(x)))
+        }
+        let set = PieChartDataSet(entries: entries)
+        set.colors = ChartColorTemplates.joyful()
+        let data = PieChartData(dataSet: set)
+        pieChart.data = data
     }
-    */
-
 }
