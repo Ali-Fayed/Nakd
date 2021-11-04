e5c2e0e08f2eda6662df444af55a14417bb6c49b
Ali Fayed <ali.fayed@fixedmea.com>
2021-11-05 01:52:27 +0200

    unit2 quiz

--- Nakd/Modules/Home Module/ActiveCollectionCategories/SecondUnit/Components/Quiz/SecondUnitQuestionBank.swift
+++ Nakd/Modules/Home Module/ActiveCollectionCategories/SecondUnit/Components/Quiz/SecondUnitQuestionBank.swift
@@ -12,34 +12,85 @@ class SecondUnitQuestionBank{
     var list = [SecondUnitQuestion]()
     
     init() {
-        list.append(SecondUnitQuestion(image: "questions", questionText: "من اهداف قراءه المعجم اللغوي ؟", choiceA: "البحث عن معنى مفردة", choiceB: "البحث عن فكرة رئيسة", choiceC: "حل مشكلة", choiceD: "البحث عن قانون", answer: 1))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "من أسئلة ماقبل القراءة :",
+                                       choiceA: "أ - ماالهدف",
+                                       choiceB: "ب ـ هل أحتاج إلى معلومات إضافية",
+                                       choiceC: "ج - هل الملخص جيد",
+                                       choiceD: "د - أين سأجد المعلومة",
+                                       answer: 1))
         
-        list.append(SecondUnitQuestion(image: "questions", questionText: "من خطوات القراءة المتعمقة استطلع ( اقرأ مطالع الفقرات لتحديد .......", choiceA: "حالة النص", choiceB: "حالة الكاتب", choiceC: "الأفكار الرئيسة", choiceD: "حالة الأديب", answer: 3))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "من خطوات القراءة المتعمقة :",
+                                       choiceA: "أ - أرسم",
+                                       choiceB: "ب - أين سأجد المعلومة",
+                                       choiceC: "ج - ألخص",
+                                       choiceD: "د - أسأل",
+                                       answer: 4))
         
-        list.append(SecondUnitQuestion(image: "questions", questionText: "من صفات الملخص الجيد :", choiceA: "يعتمد على القارئ", choiceB: "يعتمد على الكاتب", choiceC: "يعتمد على التوضيح", choiceD: "يعتمد على الرسوم والجداول", answer: 4))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "من أهم عمليات القراءة التمهيدية :",
+                                       choiceA: "أ - مطالع الفقرات",
+                                       choiceB: "ب - بنية الكتاب",
+                                       choiceC: "ج - تفاصيل الموضوع",
+                                       choiceD: "د - خاتمة الموضوع",
+                                       answer: 2))
         
-        list.append(SecondUnitQuestion(image: "questions", questionText: "الرمز ( * ) يقصد به :", choiceA: "بالإضافة إلى", choiceB: "يؤدي إلى", choiceC: "نقطة مهمة", choiceD: "يساوي", answer: 3))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "من أساليب المؤلف لمساعدة القارئ:",
+                                       choiceA: "أ - تحديد الفهرس",
+                                       choiceB: "ب - تحديد المراجع",
+                                       choiceC: "ج - تحديد الأهداف",
+                                       choiceD: "د - تحديد المضمون",
+                                       answer: 3))
         
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "من  الرسوم لتنظيم المعلومات :", choiceA: "المربعات" , choiceB: "شجرة الذاكرة" , choiceC: "المثلثات" , choiceD: "الدوائر", answer: 2))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "الهدف من قراءة القصة :",
+                                       choiceA: "أ - المتعة",
+                                       choiceB: "ب- البحث عن المعلومة",
+                                       choiceC: "ج - الإستعداد للاختبار",
+                                       choiceD: "د - زيادة الرصيد الثقافي",
+                                       answer: 1))
         
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "محمد وخالد انتهيا من قراءة كتاب عن الأسماك ، ولخصاه . تفوق محمد على خالد في التلخيص والسبب:", choiceA: "خالد تأخر في تسليم ملخص الأسماك" , choiceB: "محمد استطاع رسم الأسماك الجديدة" , choiceC: "محمد استطاع الانتهاء من الملخص سريعًا" , choiceD: "محمد نظم المعلومات على هيكل سمكة", answer: 4))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "أثناء القراءة أستطلع عن طريق :",
+                                       choiceA: "أ - الإجابة من الذاكرة",
+                                       choiceB: "ب - تلخيص الموضوع",
+                                       choiceC: "ج - وضع خط تحت الفكرة",
+                                       choiceD: "د- طرح أسئلة",
+                                       answer: 3))
         
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "يساعد المؤلف القارئ على القراءة ب", choiceA: "تحديد الأهداف" , choiceB: "تحديد قائل النص" , choiceC: "تحديد الوقت" , choiceD: "تحديد المكان", answer: 1))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "نحتاج عند القراءة للاختبار :",
+                                       choiceA: "أ - معجم",
+                                       choiceB: "ب - فهرس",
+                                       choiceC: "ج - هدوء وتركيز",
+                                       choiceD: "د - مصطلحات",
+                                       answer: 3))
         
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "القارئ في مرحلة ما قبل القراءة :", choiceA: "يحدد أهدافه القرائية" , choiceB: "يحدد الأسئلة" , choiceC: "يحدد مطالع الفقرات" , choiceD: "يحدد الإجابات", answer: 1))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "تعد قراءة العناوين الرئيسة والجانبية من خطوات :",
+                                       choiceA: "أ - القراءة المتعمقة",
+                                       choiceB: "ب - القراءة التمهيدية",
+                                       choiceC: "ج - القراءة المركزة",
+                                       choiceD: "د - القراءة الحرة",
+                                       answer: 2))
         
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "بنية الكتاب تعني :", choiceA: "الغلاف" , choiceB: "حجم الورق" , choiceC: "الفهارس وما إلى ذلك" , choiceD: "التكلفة", answer: 1))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "عند  قراءة  معجم لغوي أحتاج  إلى :",
+                                       choiceA: "أ - تدوين القانون",
+                                       choiceB: "ب - معرفة أصول الكلمة",
+                                       choiceC: "ج - كراسة خاصة",
+                                       choiceD: "د - قراءة مع صديق",
+                                       answer: 2))
         
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "من إستراتيجيات التركيز وزيادة الفهم( التسميع) .", choiceA: "صح" , choiceB: "خطأ" , choiceC: "" , choiceD: "", answer: 1))
+        list.append(SecondUnitQuestion(image: "questions",
+                                       questionText: "لو واجهك سؤال عن قانون القوة فمن الطبيعي أن تبحثي عن :",
+                                       choiceA: "أ - كتاب التوحيد",
+                                       choiceB: "ب - كتاب عن الشعر الجاهلي",
+                                       choiceC: "ج - كتاب الفيزياء",
+                                       choiceD: "د - كتاب عن الحاسب الآلي",
+                                       answer: 3))
         
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "من أسئلة ما بعد القراءة ( هل الإجابات صحيحة؟) .", choiceA: "صح" , choiceB: "خطأ" , choiceC: "" , choiceD: "", answer: 1))
-        
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "من فوائد القراءة التمهيدية السريعة إعطاء فكرة  عن حالة المؤلف .", choiceA: "صح" , choiceB: "خطأ" , choiceC: "" , choiceD: "", answer: 2))
-        
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "الإجراء المتخذ في خطوة ( راجع ) تلخيص الموضوع بأسلوب سؤال وجواب .", choiceA: "صح" , choiceB: "خطأ" , choiceC: "" , choiceD: "", answer: 1))
-        
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "من أسئلة ما قبل القراءة فحص الرسوم .", choiceA: "صح" , choiceB: "خطأ" , choiceC: "" , choiceD: "", answer: 2))
-        
-        list.append(SecondUnitQuestion(image: "questions" , questionText: "هناك بعض المعلومات لم تفهمها بعد الانتهاء من القراءة ، لكن لا بأس لن تحتاج للمساعدة فيها .", choiceA: "صح" , choiceB: "خطأ" , choiceC: "" , choiceD: "", answer: 2))
     }
 }
